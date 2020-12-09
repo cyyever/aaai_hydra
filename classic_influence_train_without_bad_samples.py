d2115a9b5f7c0ab876f6fdc826936d5d7e130d1a
cyy <cyyever@outlook.com>
2020-12-09 22:01:18 +0800

    fix code

---
+++ classic_influence_train_without_bad_samples.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+import datetime
+import json
+import os
+
+from cyy_naive_lib.algorithm.mapping_op import get_mapping_values_by_order
+from cyy_naive_lib.log import get_logger, set_file_handler
+from cyy_naive_pytorch_lib.algorithm.influence_function.classic_influence_function import \
+    compute_classic_influence_function
+from cyy_naive_pytorch_lib.arg_parse import (create_inferencer_from_args,
+                                             create_trainer_from_args,
+                                             get_arg_parser, get_parsed_args)
+from cyy_naive_pytorch_lib.dataset import split_dataset, sub_dataset
+from cyy_naive_pytorch_lib.gradient import get_dataset_gradients
+
+if __name__ == "__main__":
+    parser = get_arg_parser()
+    parser.add_argument("--model_dir", type=str)
+    args = get_parsed_args(parser)
+
+    set_file_handler(
+        os.path.join(
+            "log",
+            "classic_influence_train_without_bad_samples",
+            args.dataset_name,
+            args.model_name,
+            "{date:%Y-%m-%d_%H:%M:%S}.log".format(
+                date=datetime.datetime.now()),
+        )
+    )
+
+    trainer = create_trainer_from_args(args)
+    inferencer = create_inferencer_from_args(args)
+    model_file = "model.pt"
+    trainer.load_model(os.path.join(args.model_dir, model_file))
+    inferencer.load_model(os.path.join(args.model_dir, model_file))
+
+    training_sub_datasets = split_dataset(trainer.training_dataset)
+    training_sample_gradients = get_dataset_gradients(
+        training_sub_datasets, inferencer)
+
+    contribution_dict = compute_classic_influence_function(
+        trainer,
+        inferencer.get_gradient(),
+        training_sample_gradients,
+        batch_size=args.batch_size,
+        dampling_term=0.01,
+        scale=1000,
+        epsilon=0.03,
+    )
+
+    with open(model_file + ".classic_influence.json", "wt") as f:
+        json.dump(contribution_dict, f)
+
+    trainer = create_trainer_from_args(args)
+    contribution = sorted(list(get_mapping_values_by_order(contribution_dict)))
+    contribution = contribution[int(len(contribution) * 0.8):]
+    boundary_contribution = contribution[0]
+    positive_indices = []
+    for k, v in contribution_dict.items():
+        # if v >= boundary_contribution:
+        if v < boundary_contribution:
+            positive_indices.append(int(k))
+
+    trainer.training_dataset = sub_dataset(
+        trainer.training_dataset, positive_indices)
+
+    get_logger().info("begin train")
+    results = trainer.repeated_train(5, None, plot_class_accuracy=False)
+    get_logger().info("training_loss is %s", results["training_loss"])
+    get_logger().info("validation_loss is %s", results["validation_loss"])
+    get_logger().info(
+        "validation_accuracy is %s",
+        results["validation_accuracy"])
+    get_logger().info("test_loss is %s", results["test_loss"])
+    get_logger().info("test_accuracy is %s", results["test_accuracy"])
--- train_without_bad_samples.sh
+++ train_without_bad_samples.sh
@@ -1,3 +1,3 @@
 python3 train_without_bad_samples.py --dataset_name MNIST --model_name LeNet5 --epochs 20 --contribution_dir final_models/MNIST/LeNet5/92480fa8-ee34-415e-ab82-be227f9110b7/randomized_model
-# python3 random_train_with_hypergradient.py --dataset_name FashionMNIST --model_name LeNet5 --cache_size 4096 --epochs 20 --random_percentage 0.8
-# python3 random_train_with_hypergradient.py --dataset_name CIFAR10 --model_name DenseNet40 --cache_size 500 --epochs 20 --random_percentage 0.8
+python3 train_without_bad_samples.py --dataset_name FashionMNIST --model_name LeNet5 --epochs 20  --contribution_dir final_models/FashionMNIST/LeNet5/5dbd1cf6-a49e-489f-9ec2-c201e23e0864/randomized_model
+python3 train_without_bad_samples.py --dataset_name CIFAR10 --model_name DenseNet40  --epochs 20 --contribution_dir final_models/CIFAR10/DenseNet40/3895845a-9cd8-40d7-95ae-1892651165f4/randomized_model
