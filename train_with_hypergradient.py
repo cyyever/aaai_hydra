d26226bca0da0f57d92317e2b62fdf06473c1e49
cyy <cyyever@outlook.com>
2021-03-24 19:19:16 +0700

    fix code

--- train_with_hypergradient.py
+++ train_with_hypergradient.py
@@ -3,11 +3,8 @@ import datetime
 import os
 
 from cyy_naive_lib.log import set_file_handler
-
-from cyy_naive_pytorch_lib.algorithm.influence_function.args import (
-    add_arguments_to_parser,
-    create_hyper_gradient_trainer_from_args,
-)
+from cyy_naive_pytorch_lib.algorithm.hydra.args import (
+    add_arguments_to_parser, create_hyper_gradient_trainer_from_args)
 from cyy_naive_pytorch_lib.arg_parse import get_parsed_args
 
 if __name__ == "__main__":
@@ -20,8 +17,7 @@ if __name__ == "__main__":
             "hypergradient",
             args.dataset_name,
             args.model_name,
-            "{date:%Y-%m-%d_%H:%M:%S}.log".format(
-                date=datetime.datetime.now()),
+            "{date:%Y-%m-%d_%H:%M:%S}.log".format(date=datetime.datetime.now()),
         )
     )
     hyper_gradient_trainer = create_hyper_gradient_trainer_from_args(args)
