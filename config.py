4022c52d0a42a82a6138eea539c969cfc79054c9
cyy <cyyever@outlook.com>
2021-03-25 16:32:05 +0700

    fix code

--- config.py
+++ config.py
@@ -6,12 +6,14 @@ from cyy_naive_pytorch_lib.algorithm.hydra.hydra_config import HyDRAConfig
 class HyDRAExperimentConfig(HyDRAConfig):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        super().optimizer_name = "SGD"
+        self.optimizer_name = "SGD"
         self.label_noise_percent = None
+        # self.tracking_percentage = None
+        self.tracking_percentage = 0.01
 
     def load_args(self):
         parser = argparse.ArgumentParser()
-        parser.add_argument("--label_noise_percent", type=float, required=True)
+        parser.add_argument("--label_noise_percent", type=float, required=False)
         super().load_args(parser=parser)
 
 
