82e52779206ef565da50e4c0227d34bf2b381b3e
cyy <cyyever@outlook.com>
2021-03-26 14:10:42 +0700

    fix code

--- hypergradient_contribution.py
+++ hypergradient_contribution.py
@@ -10,21 +10,12 @@ from cyy_naive_pytorch_lib.algorithm.influence_function.hyper_gradient_analyzer 
 from cyy_naive_pytorch_lib.arg_parse import (get_inferencer_from_args,
                                              get_parsed_args)
 
+from config import get_config
+
 if __name__ == "__main__":
+    config = get_config()
 
-    parser = add_arguments_to_parser()
-    args = get_parsed_args(parser)
-
-    result_dir = os.path.join(
-        "hypergradient_contribution",
-        args.task_name,
-    )
-    os.makedirs(
-        result_dir,
-        exist_ok=True,
-    )
-
-    tester = get_inferencer_from_args(args)
+    tester = config.create_inferencer()
     analyzer = HyperGradientAnalyzer(tester, args.hyper_gradient_dir)
 
     test_subset = dict()
@@ -34,7 +25,7 @@ if __name__ == "__main__":
     contribution_dict = analyzer.get_training_sample_contributions(test_subset)
 
     with open(
-        os.path.join(result_dir, "contribution.json"),
+        os.path.join(config.get_save_dir(), "hydra_contribution.json"),
         mode="wt",
     ) as f:
         json.dump(contribution_dict, f)
