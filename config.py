c84b3acf2aed6c97f81f6d1651e5d0cdb839435c
cyy <cyyever@outlook.com>
2021-03-27 18:03:29 +0700

    fix option

--- config.py
+++ config.py
@@ -7,14 +7,12 @@ class HyDRAExperimentConfig(HyDRAConfig):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.optimizer_name = "SGD"
-        self.label_noise_percent = None
         # self.tracking_percentage = None
-        self.tracking_percentage = 0.001
+        self.tracking_percentage = 0.01
 
     def load_args(self, parser=None):
         if parser is None:
             parser = argparse.ArgumentParser()
-        parser.add_argument("--label_noise_percent", type=float, required=False)
         super().load_args(parser=parser)
 
 
