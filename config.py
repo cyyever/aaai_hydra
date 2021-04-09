818146955b5a8b3c8637edd1ccb8c21194d8995a
cyy <cyyever@outlook.com>
2021-04-09 16:20:23 +0800

    fix code

--- config.py
+++ config.py
@@ -4,9 +4,8 @@ from cyy_naive_pytorch_lib.algorithm.hydra.hydra_config import HyDRAConfig
 class HyDRAExperimentConfig(HyDRAConfig):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.optimizer_name = "SGD"
-        # self.tracking_percentage = None
-        self.tracking_percentage = 0.01
+        self.hyper_parameter_config.optimizer_name = "SGD"
+        self.tracking_percentage = None
 
 
 def get_config(parser=None) -> HyDRAExperimentConfig:
