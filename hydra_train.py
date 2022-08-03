ba72e5d6cca91e62078a7349f59bd140d5f78988
cyy <cyyever@outlook.com>
2022-08-03 10:57:47 +0800

    add code

--- hydra_train.py
+++ hydra_train.py
@@ -2,12 +2,27 @@
 import datetime
 import os
 
+import hydra
 from cyy_naive_lib.log import set_file_handler
 from cyy_torch_algorithm.hydra.hydra_config import HyDRAConfig
 
+global_config = HyDRAConfig()
+
+remain_config = None
+
+
+@hydra.main(config_path="conf", version_base=None)
+def load_config(conf):
+    global remain_config
+    if len(conf) == 1:
+        conf = next(iter(conf.values()))
+    remain_config = HyDRAConfig.load_config(global_config, conf, check_config=True)
+    assert not remain_config
+
+
 if __name__ == "__main__":
-    config = HyDRAConfig()
-    config.load_args()
+    load_config()
+    config = global_config
     set_file_handler(
         os.path.join(
             "log",
