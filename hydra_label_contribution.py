59ccafe218e273aeef130557c51d281dea50aac6
cyy <cyyever@outlook.com>
2022-08-15 11:05:27 +0800

    add code

--- hydra_label_contribution.py
+++ hydra_label_contribution.py
@@ -10,25 +10,23 @@ import sys
 import matplotlib
 import numpy
 import seaborn
+from cyy_naive_lib.algorithm.mapping_op import get_mapping_values_by_key_order
 from cyy_naive_lib.log import get_logger, set_file_handler
-from cyy_torch_toolbox.dataset import DatasetUtil
-from cyy_torch_toolbox.ml_type import MachineLearningPhase
 from cyy_torch_algorithm.hydra.hydra_analyzer import HyDRAAnalyzer
 from cyy_torch_algorithm.hydra.hydra_config import HyDRAConfig
 from cyy_torch_algorithm.normalization import normalize_for_heatmap
+from cyy_torch_toolbox.ml_type import MachineLearningPhase
+from cyy_torch_toolbox.trainer import Trainer
 
 matplotlib.use("Agg")
 
 
 def compute_distribution(
     config,
-    tester,
+    trainer: Trainer,
     indices=None,
     prefix="whole",
 ):
-    training_dataset = tester.dataset_collection.get_dataset(
-        MachineLearningPhase.Training
-    )
     training_set_size = None
     with open(
         os.path.join(config.hydra_dir, "training_set_size"),
@@ -36,32 +34,38 @@ def compute_distribution(
     ) as f:
         training_set_size = pickle.load(f)
     analyzer = HyDRAAnalyzer(
-        tester,
+        trainer.get_inferencer(),
         os.path.join(config.hydra_dir, "approximation_hyper_gradient_dir"),
         training_set_size,
     )
 
     if prefix == "abnormal":
-        indices = set(range(len(training_dataset))) - indices
+        indices = set(range(training_set_size)) - indices
 
-    training_subset = dict()
-    for label, label_dataset in DatasetUtil(training_dataset).split_by_label().items():
+    training_subset = {}
+    dataset_util = trainer.dataset_collection.get_dataset_util(
+        MachineLearningPhase.Training
+    )
+    for label, label_indices in dataset_util.label_sample_dict.items():
         get_logger().info("compute label %s", label)
-        training_subset[label] = set(label_dataset["indices"])
+        training_subset[label] = set(label_indices)
         if indices is not None:
             training_subset[label] &= set(indices)
 
-    test_subset = dict()
-    for label, label_dataset in DatasetUtil(tester.dataset).split_by_label().items():
+    test_subset = {}
+    dataset_util = trainer.dataset_collection.get_dataset_util(
+        MachineLearningPhase.Test
+    )
+    for label, label_dataset in dataset_util.label_sample_dict.items():
         get_logger().info("compute label %s", label)
-        test_subset[label] = set(label_dataset["indices"])
+        test_subset[label] = set(label_dataset)
 
     subset_contribution_dict = analyzer.get_subset_contributions(
         training_subset, test_subset
     )
-    means = dict()
+    means = {}
     for training_label, tmp in subset_contribution_dict.items():
-        means[training_label] = dict()
+        means[training_label] = {}
         for test_label, v in tmp.items():
             means[training_label][test_label] = v / (
                 len(training_subset[training_label]) * len(test_subset[test_label])
@@ -128,7 +132,7 @@ def compute_distribution(
     mean_array = mean_array - mean_array.mean()
     mean_array = mean_array / mean_array.std()
 
-    label_list = tester.dataset_collection.get_label_names()
+    label_list = get_mapping_values_by_key_order(dataset_util.get_label_names())
     if config.dataset_name == "MNIST":
         label_list = [" " * 18 + str(a) for a in label_list]
     mean_array = normalize_for_heatmap(mean_array)
@@ -151,30 +155,29 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--hydra_dir", type=str, required=True)
     config = HyDRAConfig()
-    config.load_args(parser)
 
     set_file_handler(
         os.path.join(
             "log",
             "hydra_distribution",
             config.dc_config.dataset_name,
-            config.model_name,
+            config.model_config.model_name,
             "{date:%Y-%m-%d_%H:%M:%S}.log".format(date=datetime.datetime.now()),
         )
     )
-    tester = config.create_inferencer()
+    trainer = config.create_trainer()
     randomized_label_map = config.dc_config.training_dataset_label_map
     if randomized_label_map is None:
-        compute_distribution(config, tester)
+        compute_distribution(config, trainer)
         sys.exit(0)
 
     compute_distribution(
-        config, tester, indices=randomized_label_map.keys(), prefix="abnormal"
+        config, trainer, indices=randomized_label_map.keys(), prefix="abnormal"
     )
 
     compute_distribution(
         config,
-        tester,
+        trainer,
         indices=randomized_label_map.keys(),
         prefix="normal",
     )
