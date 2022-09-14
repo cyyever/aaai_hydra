8d0f6f5dc2fb9b6311b0f247dac3913ca4116df4
cyy <cyyever@outlook.com>
2022-09-14 15:07:17 +0800

    add lean_hydra

---
+++ lean_hydra_train.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import datetime
+import os
+
+import hydra
+from cyy_naive_lib.log import set_file_handler
+from cyy_torch_algorithm.lean_hydra.lean_hydra_config import LeanHyDRAConfig
+
+config = LeanHyDRAConfig()
+
+
+@hydra.main(config_path="conf", version_base=None)
+def load_config(conf):
+    global config
+    if len(conf) == 1:
+        conf = next(iter(conf.values()))
+    LeanHyDRAConfig.load_config(config, conf, check_config=False)
+
+
+if __name__ == "__main__":
+    load_config()
+    set_file_handler(
+        os.path.join(
+            "log",
+            config.dc_config.dataset_name,
+            config.model_config.model_name,
+            "{date:%Y-%m-%d_%H_%M_%S}.log".format(date=datetime.datetime.now()),
+        )
+    )
+    lean_hydra_trainer = config.create_deterministic_trainer()
+    lean_hydra_trainer.train()
+    lean_hydra_trainer, hook, test_gradient = config.recreate_trainer_and_hook()
+    lean_hydra_trainer.train()
