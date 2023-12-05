38e29ab12fbaac80bd4cb9ed12d1a29768b6a26f
cyy <cyyever@outlook.com>
2023-12-05 12:44:33 +0800

    Fix config and script

--- hydra_train.sh
+++ hydra_train.sh
@@ -1,4 +1,3 @@
-python3 lean_hydra_train.py --config-name mnist.yaml
-# python3 hydra_train.py --config-name mnist.yaml
+python3 hydra_train.py --config-name mnist.yaml
 # python3 hydra_train.py --config-name fashion_mnist.yaml
 # python3 hydra_train.py --config-name cifar10.yaml
