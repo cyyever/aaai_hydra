89f49658dc5bb1e2b7ec1143b9b58ecb314848f8
cyy <cyyever@outlook.com>
2024-11-24 10:05:40 +0800

    Fix code

--- util.py
+++ util.py
@@ -3,11 +3,11 @@ import os
 
 import torch
 from cyy_naive_lib.algorithm.mapping_op import get_mapping_values_by_key_order
-from cyy_naive_lib.log import get_logger
-from cyy_torch_toolbox.dataset import subset_dp
-from cyy_torch_toolbox.executor import Executor
-from cyy_torch_toolbox.inferencer import Inferencer
-from cyy_torch_toolbox.ml_type import MachineLearningPhase
+from cyy_naive_lib.log import log_info
+from cyy_torch_toolbox import subset_dp
+from cyy_torch_toolbox import Executor
+from cyy_torch_toolbox import Inferencer
+from cyy_torch_toolbox import MachineLearningPhase
 
 
 def analysis_contribution(
@@ -20,16 +20,12 @@ def analysis_contribution(
     max_contribution = torch.max(contribution).item()
     min_contribution = torch.min(contribution).item()
 
-    get_logger().info("std is %s", std)
-    get_logger().info("mean is %s", mean)
-    get_logger().info("max contribution is %s", max_contribution)
-    get_logger().info("min contribution is %s", min_contribution)
-    get_logger().info(
-        "positive contributions have %s", contribution[contribution >= 0].shape
-    )
-    get_logger().info(
-        "negative contributions have %s", contribution[contribution < 0].shape
-    )
+    log_info("std is %s", std)
+    log_info("mean is %s", mean)
+    log_info("max contribution is %s", max_contribution)
+    log_info("min contribution is %s", min_contribution)
+    log_info("positive contributions have %s", contribution[contribution >= 0].shape)
+    log_info("negative contributions have %s", contribution[contribution < 0].shape)
 
     positive_contributions: dict = {
         k: v for k, v in contribution_dict.items() if v > (max_contribution * threshold)
