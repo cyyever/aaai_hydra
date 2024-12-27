889aa1f6f1c875247512f994ef0115550705b9b3
cyy <cyyever@outlook.com>
2024-12-27 18:32:39 +0800

    Fix code

--- contribution_analysis.py
+++ contribution_analysis.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import json
 
 import hydra
@@ -24,7 +22,8 @@ def load_config(conf):
 
 if __name__ == "__main__":
     load_config()
-    trainer = config.create_trainer()
+    trainer = config.create_trainer()["trainer"]
+    assert other_config is not None
 
     with open(other_config["contribution_path"], encoding="utf8") as f:
         contribution_dict = {int(k): v for k, v in json.load(f).items()}
